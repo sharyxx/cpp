f004ac3dba29a0decf04465b457fd6efdcba9abd
sharyxx <minkhantzaw38@gmail.com>
2017-11-09 11:49:40 +0000

    Passed Test with dynamic array

--- hrank/array_add.cpp
+++ hrank/array_add.cpp
@@ -1,1 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+	int *a = NULL;
+	int b;
+	cin >> b;
+	int temp = 0;
+	a = new  int[b];
+
+	for (int i = 0; i<b; i++) {
+
+		cin >> a[i];
+		temp += a[i];
+
+	}
+
+	cout << temp;
+
+	delete[] a;
+}
