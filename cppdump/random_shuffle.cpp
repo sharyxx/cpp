25da6f37e54910477f662cfbf6531b5e0b931897
sharyxx <minkhantzaw38@gmail.com>
2017-11-21 07:15:29 +0000

    added cppdump and randomshuffle

---
+++ cppdump/random_shuffle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main (){
+
+int cornerMoves[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};// {'1', '2', '3', '4','5','6','7','8','9','10','11','12','13','14','15','16'};
+random_shuffle(&cornerMoves[0], &cornerMoves[15]);
+
+for (int i = 0 ; i < 10 ; i++){
+
+	cout<< cornerMoves[i]<<" ";
+}
+
+}
